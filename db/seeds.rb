42865466d1d280fea9ed24bc3af42b97ceb0bfc4
Bruno Montebello <brunormontebello@hotmail.com>
2020-11-03 00:33:34 +0000

    Seed the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+airlines = Airline.create([
+  {
+    name: "United Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+  },
+  {
+    name: "Alaska Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png"
+  },
+  {
+    name: "American Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png"
+  },
+  {
+    name: "Delta",
+    image_url: "https://open-flights.s3.amazonaws.com/Delta.png"
+  },
+  {
+    name: "JetBlue",
+    image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png"
+  },
+  {
+    name: "Southwest",
+    image_url: "https://open-flights.s3.amazonaws.com/Southwest.png"
+  }
+])
+
+reviews = Review.create([
+  {
+    title: 'Great Airline',
+    description: 'I had a great flight',
+    score: 5,
+    airline: airlines.first
+  },
+    {
+    title: 'Ok Airline',
+    description: 'I had an ok flight',
+    score: 3,
+    airline: airlines.first
+  },
+    {
+    title: 'Bad Airline',
+    description: 'I had a bad flight',
+    score: 1,
+    airline: airlines.first
+  }
+])
